5c3f42d2dfca698b23213d73570f94a1f6c34816
SrunChina <china.srunn@gmail.com>
2023-03-05 16:30:37 +0700

    first commit

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import "dotenv/config";
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
